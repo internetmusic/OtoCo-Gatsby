16429e3f4499f4fcc36daba8a77d30c6d282d8f0
Filipe Soccol <filipe.soccol@gmail.com>
2021-01-13 11:35:15 -0300

    Remove the Gatsby Offline plugin that was caching app for clients.

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,6 @@ module.exports = {
     //   },
     // },
     'gatsby-plugin-catch-links',
-    'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
